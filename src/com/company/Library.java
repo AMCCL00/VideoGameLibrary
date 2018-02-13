890f814b7b73d54feecefb6be6cdd1156acca1bb
Andrew McClary <andrew.mccalry00@gmail.com>
2018-02-13 16:06:34 -0500

    Finished Project

--- src/com/company/Library.java
+++ src/com/company/Library.java
@@ -1,16 +1,73 @@
 package com.company;
 
-import java.util.Scanner;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class Library {
+
+    private final Menu menu;
+    private List<Game> gameLibrary = new ArrayList<Game>();
+    private List<Game> checkedOutGames = new ArrayList<Game>();
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
     Scanner input = new Scanner(System.in);
 
-    protected void addGame(){
+    public Library(Menu menu) {
+        this.menu = menu;
+    }
+
+    protected void addGame(Game game) {
         //Code foes here to add game to Library List
-        System.out.println("What is the title of the game you would like to add?");
-        String title = input.nextLine();
+        gameLibrary.add(game);
+        menu.startMenu();
+    }
 
-        System.out.println(title + " has been added to the Game Library");
+    protected void removeGame(int gameIndex) {
+        gameIndex -- ;
+        gameLibrary.remove(gameIndex);
+        System.out.println("The selected game has been removed from your library.\n");
+        menu.startMenu();
 
+    }
+
+    protected void checkoutGame(int gameIndex) {
+        gameIndex --;
+        Game game = gameLibrary.get(gameIndex);
+
+        checkedOutGames.add(game);
+
+        Calendar calendar = Calendar.getInstance();
+        System.out.println("You Checked out this game on: " + dateFormat.format(calendar.getTime()));
+        calendar.add(Calendar.DAY_OF_YEAR, 3);
+        System.out.println("The due date of the game is " + dateFormat.format(calendar.getTime()) + "\n");
+        game.setDueDate(dateFormat.format(calendar.getTime()));
+
+        gameLibrary.remove(gameIndex);
+        menu.startMenu();
+    }
+
+    protected void checkInGame(int gameIndex) {
+         gameIndex --;
+        Game game = checkedOutGames.get(gameIndex);
+
+        System.out.println("Your game has been checked back in.\n");
+        checkedOutGames.remove(game);
+
+        menu.startMenu();
+    }
+
+    protected void checkedOutGameList() {
+        System.out.println("Your currently checked out games are: ");
+        for (int i = 0; i < checkedOutGames.size(); i++){
+            Game game = checkedOutGames.get(i);
+            System.out.println((i + 1) + ". " + game.getTitle());
+        }
+    }
+
+    protected void libraryList() {
+        System.out.println("The games currently in your library are: ");
+        for (int i = 0; i < gameLibrary.size(); i++) {
+            Game game = gameLibrary.get(i);
+            System.out.println((i + 1) + ". " + game.getTitle());
+        }
     }
 }
--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class Menu {
 
-    Scanner input = new Scanner(System.in);
+    private Scanner input = new Scanner(System.in);
+    private Library library = new Library(this);
 
     public void startMenu() {
         System.out.println("Welcome to the  Video Game library, what would you like to do?");
@@ -21,21 +22,50 @@ public class Menu {
             switch (input.nextInt()) {
                 case 1:
                     //Add
+                    input.nextLine();
+                    System.out.println("You have chosen to add a game.\n" +
+                            "What is the title of this game?");
+                    Game game = new Game(input.nextLine());
+                    System.out.println("You have added " + game.getTitle() + " to your library, this game can now be checked out.\n");
+                    library.addGame(game);
                     break;
                 case 2:
                     //remove
+                    System.out.println("You have chosen to remove a game from your library. \n" +
+                            "Here are a list of games, type the one you would like to remove.");
+                    library.libraryList();
+                    //create method to list all games that can be removed
+                    library.removeGame(input.nextInt());
                     break;
                 case 3:
                     //View Library
+                    library.libraryList();
+                    System.out.println(" ");
+                    startMenu();
                     break;
                 case 4:
                     //Check out Game
+                    System.out.println("You have chosen to check out a game. \n" +
+                            "Here are a list of your games, which would you like to check out?");
+                    library.libraryList();
+                    //use the method from the removed games list for this list
+                    library.checkoutGame(input.nextInt());
+                    System.out.println(" ");
                     break;
                 case 5:
                     //Check in Game
+                    System.out.println("You have selected to check a game in \n" +
+                            "Here is a list of your checked out games, which would you like to check in?");
+                    library.checkedOutGameList();
+                    //use the list from checked out games
+                    library.checkInGame(input.nextInt());
+                    System.out.println(" ");
                     break;
                 case 6:
                     //View checked out game
+                    library.checkedOutGameList();
+                    System.out.println(" ");
+                    startMenu();
                     break;
                 case 7:
                     //Exit
@@ -43,12 +73,12 @@ public class Menu {
                     break;
                 default:
                     input.nextLine();
-                    System.out.println("Please pick a number between 1 and 7.");
+                    System.out.println("Please pick a number between 1 and 7.\n");
                     startMenu();
             }
         }catch (Exception e){
             input.nextLine();
-            System.out.println("Please pick a number between 1 and 7.");
+            System.out.println("Please pick a number between 1 and 7.\n");
             startMenu();
         }
     }
